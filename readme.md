20cb8a560f37556a96af8e5a2e6d35c81c4c4f60
fengyu_u <395861552@qq.com>
2018-01-06 19:16:24 +0800

    0106

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 # task116_scoresheet-cli
+
+判断格式还没加，打印中文名字有点问题
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -10,3 +10,28 @@ describe('输入信息跳转到相应模块', () => {
     });
 
 });
+
+describe('打印', () => {
+
+    it('should display main menu once started', () => {
+        sinon.spy(console, 'log');
+        main();
+        expect(console.log.args.join()).toBe(`成绩单
+姓名|数学|语文|英语|平均分|总分
+========================
+
+zhangsan|89|98|79|88.67|266
+
+lisi|56|78|89|74.33|223
+
+fhdiuf|56|78|89|74.33|223
+
+========================
+全班总分平均数：237.33
+全班总分中位数：223
+`);
+    });
+
+});
+
+//zhangsan,10001,89,98,79   lisi,10002,56,78,89      fhdiuf,10003,56,78,89
